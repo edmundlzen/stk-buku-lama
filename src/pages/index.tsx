5f57f3b3fbcc47136a9ffac5d1abcdff458bd9ca
Edmund <edmundlzen@gmail.com>
2024-02-21 17:25:48 +0800

    Refactor API and fetch data from Google Sheets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,39 @@
 import Head from "next/head";
-import { Item } from "./api/get-items";
 import Image from "next/image";
 import moment from "moment";
+import doc from "~/utils/doc";
 
-// export type Item = {
-//   timestamp: string;
-//   name: string;
-//   class: string;
-//   phone: string;
-//   book: string;
-//   image: string;
-//   condition: string;
-//   price: string;
-//   sold: string;
-// };
+export type Item = {
+  timestamp: string;
+  name: string;
+  class: string;
+  phone: string;
+  book: string;
+  image: string;
+  condition: string;
+  price: string;
+  sold: string;
+};
 
 export async function getStaticProps() {
-  const items = (await fetch(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/get-items"
-      : "https://stk-buku-lama.vercel.app/api/get-items",
-  ).then((res) => res.json())) as { items: Item[] };
+  // Get items
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet?.getRows();
+  const filteredRows = rows?.filter((row) => row?.get("Timestamp") !== "");
 
   return {
     props: {
-      items: items.items,
+      items: filteredRows?.map((row) => ({
+        timestamp: row?.get("Timestamp") as string,
+        name: row?.get("Nama") as string,
+        class: row?.get("Kelas") as string,
+        phone: row?.get("Nombor Telefon") as string,
+        book: row?.get("Nama buku yang jual") as string,
+        image: row?.get("Gambar buku") as string,
+        condition: row?.get("Keadaan buku") as string,
+        price: row?.get("Harga") as string,
+        sold: row?.get("Sold") as string,
+      })) as Item[],
     },
     revalidate: 5,
   };
